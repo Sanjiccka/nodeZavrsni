dd7c876d11fb4a0ba768f58851031b7ecf3c9fa2
Sanjiccka <44682454+Sanjiccka@users.noreply.github.com>
2019-05-16 12:50:59 +0200

    Add files via upload

--- models/users.js
+++ models/users.js
@@ -1,9 +1,23 @@
-exports.create = function (fn, ln, email, bd, tel, country, pass) {
-    this.fn = fn ;
-    this.ln = ln ;
-    this.email = email ;
-    this.bd = bd ;
-    this.tel = tel;
-    this.country = country;
-    this.pass = pass ;
-}
+// exports.create = function (fn, ln, email, bd, tel, country, pass) {
+//     this.fn = fn ;
+//     this.ln = ln ;
+//     this.email = email ;
+//     this.bd = bd ;
+//     this.tel = tel;
+//     this.country = country;
+//     this.pass = pass ;
+// }
+
+
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+var userSchema = new Schema ({
+    fn: {type:String, required: true},
+    ln: {type:String, required: true},
+    email: { type:String, required:true},
+    bd: {type:Date, required:true},
+    tel: {type:Number, required:true},
+    country: {type:String, required:true},
+    pass: { type:String, required:true}
+})
